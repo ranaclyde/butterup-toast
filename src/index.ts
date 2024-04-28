14207a5e1dd4f8bf2c4a0d0c76c309db924eeef3
Matías E Sanhueza <manoloesanhueza@gmail.com>
2024-04-28 12:45:59 -0300

    Add types

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
-export function sayHello(): void {
-  //
+import type { ButterupProps } from './types'
+
+export const butterup: ButterupProps = {
+  options: {
+    maxToasts: 3,
+    toastLife: 3000,
+    currentToasts: 0,
+    stackedToasts: true
+  },
+  toast({ title, message, type, location, icon, theme, dismissible }) {},
+  despawnToast(toastId) {}
 }
---
+++ src/types.ts
@@ -0,0 +1,35 @@
+// Based on https://github.com/midudev/la-velada-web-oficial/blob/main/src/function/toast.ts
+
+/* eslint-disable @typescript-eslint/indent */
+export interface ToastOptions {
+  title: string
+  message: string
+  type?: 'success' | 'error' | 'warning' | 'info'
+  location?:
+    | 'top-right'
+    | 'top-center'
+    | 'top-left'
+    | 'bottom-right'
+    | 'bottom-center'
+    | 'bottom-left'
+  icon?: boolean
+  theme?: string
+  dismissible?: boolean
+}
+
+export interface ButterupProps {
+  options: ButterupOptions
+  toast: (toast: ToastOptions) => void
+  despawnToast: (id: string) => void
+}
+
+export interface ButterupOptions {
+  /** Max number of toasts that can be on the screen at once */
+  maxToasts: number
+  /** How long a toast will stay on the screen before fading away */
+  toastLife: number
+  /** Current number of toasts on the screen */
+  currentToasts: number
+  /** Whether or not to stack toasts */
+  stackedToasts: boolean
+}
